974e71edacdf73d4ed866bf77916de7916818f16
viktor-gif <klim.victor.ua@gmail.com>
2021-04-30 00:09:05 +0300

    Lesson 1: the first stream

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-// import { Rx } from "rxjs/Rx";
+let stream$ = rxjs.Observable.create(function (observer) {
+  console.log("Stream was created");
+  observer.next("One");
 
-console.log(rxjs);
+  setTimeout(function () {
+    observer.error("Something went wrong!");
+  }, 5000);
+  setTimeout(function () {
+    // observer.complete();
+  }, 3000);
+  setTimeout(function () {
+    observer.next("After 2 seconds!");
+  }, 2000);
+  //   observer.complete();
+  observer.next("Two");
+});
+
+stream$.subscribe(
+  function (data) {
+    console.log("subscribe: ", data);
+  },
+  function (error) {
+    console.log("Error: ", error);
+  },
+  function () {
+    console.log("Completed!");
+  }
+);
