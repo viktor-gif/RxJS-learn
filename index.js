c72e04cd1964c66b562c43ad6caa9391cf34fce3
viktor-gif <klim.victor.ua@gmail.com>
2021-05-05 21:32:44 +0300

    Lesson 3: of

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
-// let button = document.querySelector("button");
-// console.log(rxjs);
-// let btn$ = rxjs.Observable.fromEvent(button, "click");
-
-// console.log(btn$);
-
-// btn$._subscribe(function (e) {
-//   console.log(e);
-// });
-
-let button = document.querySelector("button");
-
-let btn$ = rxjs.fromEvent(button, "click");
-
-btn$.subscribe(function (e) {
-  console.log(e);
-});
+function createSubscribe(name) {
+  return {
+    next(x) {
+      console.log(name, ": ", x);
+    },
+    error(err) {
+      console.log("Error: ", err);
+    },
+    complete() {
+      console.log(name, ": Completed.");
+    },
+  };
+}
 
 rxjs
-  .fromEvent(document.querySelector("input"), "keyup")
-  .subscribe((e) => console.log(e));
+  .of([5, 3], 0, 5, "string", true, { name: "Vasya" })
+  .subscribe(createSubscribe("of"));
 
-rxjs.fromEvent(document, "mousemove").subscribe((e) => {
-  document.querySelector("h1").innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
-});
-
-// let s$ = rxjs.Observable.create(function (observer) {
-//   observer.next("One");
-
-//   setTimeout(function () {
-//     observer.next("After 5 seconds!");
-//   }, 5000);
-
-//   setTimeout(function () {
-//     observer.next("After 3 seconds!");
-//   }, 3000);
-
-//   setTimeout(function () {
-//     observer.next("After 2 seconds!");
-//   }, 2000);
-//   observer.next("Two");
-// });
-
-// s$.subscribe(function (data) {
-//   console.log("subscribe: ", data);
-// });
+// rxjs.of([5, 3], 0, 5, "string", true, { name: "Vasya" }).subscribe(
+//   (x) => console.log("Next: ", x),
+//   (err) => console.log("Error: ", err),
+//   () => console.log("Completed.")
+// );
