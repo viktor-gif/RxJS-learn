3e7d24ba123bb41375d57fc17c8f08c039f1af18
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 16:12:17 +0300

    Lesson 14: error processing (catchError(ficing))

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function createSubscribe(name) {
       console.log(name, ": ", x);
     },
     error(err) {
-      console.log("Error: ", err);
+      console.log(name, ": ", err);
     },
     complete() {
       console.log(name, ": Completed.");
@@ -17,6 +17,6 @@ const ops = rxjs.operators;
 rxjs
   .throwError(new Error("Что-то пошло не так!"))
   .pipe(ops.catchError((error) => rxjs.of(error)))
-  .subscribe((x) => console.log("X: ", x));
+  .subscribe(createSubscribe("catchError"));
 
 rxjs.interval(500).pipe(ops.take(3)).subscribe(createSubscribe("interval"));
