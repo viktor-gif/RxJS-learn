b75c1b99e9a486733a000f959cc63f833946b27f
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 14:16:19 +0300

    Lesson 11: mergeAll

--- index.js
+++ index.js
@@ -16,7 +16,15 @@ const ops = rxjs.operators;
 
 // совмещение разных стримов в один стрим
 
-const s1$ = rxjs.interval(1000).pipe(ops.map((x) => "Stream 1: " + x));
-const s2$ = rxjs.interval(500).pipe(ops.map((x) => "Stream 2: " + x));
+// const s1$ = rxjs.interval(1000).pipe(ops.map((x) => "Stream 1: " + x));
+// const s2$ = rxjs.interval(500).pipe(ops.map((x) => "Stream 2: " + x));
 
-rxjs.merge(s1$, s2$).pipe(ops.take(12)).subscribe(createSubscribe("merge"));
+// rxjs.merge(s1$, s2$).pipe(ops.take(12)).subscribe(createSubscribe("merge"));
+
+rxjs
+  .range(1, 3)
+  .pipe(
+    ops.map((x) => rxjs.range(1, 3)),
+    ops.mergeAll()
+  )
+  .subscribe(createSubscribe("mergeAll"));
