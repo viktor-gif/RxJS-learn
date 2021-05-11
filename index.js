c6086963990f4cbd7e9a6321b8da4a6f576c68ec
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 15:11:29 +0300

    Lesson 13: zip

--- index.js
+++ index.js
@@ -16,15 +16,13 @@ const ops = rxjs.operators;
 
 // совмещение разных стримов в один стрим
 
+const interval$ = rxjs.interval(1000);
 rxjs
-  .range(1, 10)
-  .pipe(
-    ops.concatMap((x, i) => {
-      //i - это индекс
-      return rxjs.interval(100).pipe(
-        ops.take(x),
-        ops.map((q) => i)
-      );
-    })
-  )
-  .subscribe(createSubscribe("concatMap"));
+  .zip(interval$, interval$.pipe(ops.take(3)), rxjs.of("wmf"))
+  //take(3) - выдаст 3 значения, of('wmf') - выдаст одно значение
+  .subscribe(createSubscribe("zip"));
+
+// const s1$ = rxjs.of("Hello");
+// const s2$ = rxjs.of("World");
+
+// rxjs.zip(s1$, s2$.pipe(ops.delay(5000))).subscribe(createSubscribe("zip"));
