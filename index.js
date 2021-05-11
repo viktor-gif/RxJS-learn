56e58344875b034ce6cf13a72bbf38cfb59facc6
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 15:26:51 +0300

    Lesson 13: withLatestFrom, combineLatest

--- index.js
+++ index.js
@@ -16,13 +16,18 @@ const ops = rxjs.operators;
 
 // совмещение разных стримов в один стрим
 
-const interval$ = rxjs.interval(1000);
+const t1$ = rxjs.timer(1000, 2000);
+const t2$ = rxjs.timer(2000, 2000);
+const t3$ = rxjs.timer(3000, 2000);
+const t4$ = rxjs.timer(4000, 2000);
+
 rxjs
-  .zip(interval$, interval$.pipe(ops.take(3)), rxjs.of("wmf"))
-  //take(3) - выдаст 3 значения, of('wmf') - выдаст одно значение
-  .subscribe(createSubscribe("zip"));
+  .combineLatest(t1$, t2$, t3$, t4$)
+  .pipe(ops.take(10))
+  .subscribe(createSubscribe("combineLatest"));
 
-// const s1$ = rxjs.of("Hello");
-// const s2$ = rxjs.of("World");
-
-// rxjs.zip(s1$, s2$.pipe(ops.delay(5000))).subscribe(createSubscribe("zip"));
+// const int1$ = rxjs.interval(1000);
+// const int2$ = rxjs.interval(500);
+// int1$
+//   .pipe(ops.take(5), ops.withLatestFrom(int2$))
+//   .subscribe(createSubscribe("withLatestFrom"));
