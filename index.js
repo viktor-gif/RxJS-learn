20564a4a7323080a4710f56e45b4b0dcb6bc72f4
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 15:53:25 +0300

    Lesson 14: error processing (throwError)

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ function createSubscribe(name) {
 
 const ops = rxjs.operators;
 
-// совмещение разных стримов в один стрим
-
-const t1$ = rxjs.timer(1000, 2000);
-const t2$ = rxjs.timer(2000, 2000);
-const t3$ = rxjs.timer(3000, 2000);
-const t4$ = rxjs.timer(4000, 2000);
-
 rxjs
-  .combineLatest(t1$, t2$, t3$, t4$)
-  .pipe(ops.take(10))
-  .subscribe(createSubscribe("combineLatest"));
-
-// const int1$ = rxjs.interval(1000);
-// const int2$ = rxjs.interval(500);
-// int1$
-//   .pipe(ops.take(5), ops.withLatestFrom(int2$))
-//   .subscribe(createSubscribe("withLatestFrom"));
+  .throwError(new Error("Что-то пошло не так!"))
+  .subscribe((x) => console.log(x));
