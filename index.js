79ffc41d2393b4b56ab2bb436f9dd131193df591
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 11:24:48 +0300

    Lesson 8: operators for filtering (filter with example)

--- index.js
+++ index.js
@@ -14,15 +14,28 @@ function createSubscribe(name) {
 
 const ops = rxjs.operators;
 
-// rxjs
-//   .interval(500)
-//   .pipe(
-//     ops.skipWhile((x) => x < 5),
-//     ops.takeWhile((x) => x < 12)
-//   )
-//   .subscribe(createSubscribe("skipWhile"));
+const cars = [
+  { name: "audi", price: 500 },
+  { name: "bmv", price: 400 },
+  { name: "ford", price: 450 },
+];
 
 rxjs
-  .interval(500)
-  .pipe(ops.skipUntil(rxjs.timer(3000)), ops.takeUntil(rxjs.timer(5000)))
-  .subscribe(createSubscribe("skipWhile"));
+  .fromEvent(document.querySelector("input"), "keyup")
+  .pipe(ops.map((e) => e.target.value))
+
+  .subscribe((x) => {
+    rxjs
+      .from(cars)
+      .pipe(ops.filter((c) => c.name === x))
+      .subscribe((v) => {
+        document.querySelector(
+          "div"
+        ).innerHTML = `<h2>${v.name.toUpperCase()}</h2><h4>${v.price}</h4>`;
+      });
+  });
+
+// rxjs
+//   .range(0, 10)
+//   .pipe(ops.filter((x) => x > 3))
+//   .subscribe(createSubscribe("filter"));
