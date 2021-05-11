e98d2c7a6b0f8906f45e826e88c50cfd97d7e6a0
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 14:34:00 +0300

    Lesson 12: mergeMap, example not using mergeMap

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ const ops = rxjs.operators;
 
 // совмещение разных стримов в один стрим
 
-// const s1$ = rxjs.from([1, 2, 3]);
-// const s2$ = rxjs.from([4, 5, 6]);
-// rxjs.concat(s1$, s2$).subscribe(createSubscribe("concat"));
-
-const s1$ = rxjs
-  .range(1, 3)
+rxjs
+  .of("Hello")
   .pipe(
-    ops.map((x) => rxjs.range(x, 3)),
-    ops.concatAll()
+    ops.mergeMap((x) => {
+      return rxjs.of(x + " world");
+    })
   )
-  .subscribe(createSubscribe("concatAll"));
+  .subscribe(createSubscribe("mergeMap"));
+
+// rxjs.of("Hello").subscribe((x) => {
+//   rxjs.of(x + " world!").subscribe(createSubscribe("mergeMap"));
+// });
