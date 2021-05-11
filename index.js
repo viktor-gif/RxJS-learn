0ef37940a308402a7b4ffa0cf8d86e1b39ee8886
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 16:08:26 +0300

    Lesson 14: error processing (catchError)

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ const ops = rxjs.operators;
 
 rxjs
   .throwError(new Error("Что-то пошло не так!"))
-  .subscribe((x) => console.log(x));
+  .pipe(ops.catchError((error) => rxjs.of(error)))
+  .subscribe((x) => console.log("X: ", x));
+
+rxjs.interval(500).pipe(ops.take(3)).subscribe(createSubscribe("interval"));
