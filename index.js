13e6ccc3aa6957ff6e173971ff0fb00805ff7320
viktor-gif <klim.victor.ua@gmail.com>
2021-05-11 14:11:38 +0300

    Lesson 11: merge (example)

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const ops = rxjs.operators;
 
 // совмещение разных стримов в один стрим
 
-const s1$ = rxjs.of("Hello ");
-const s2$ = rxjs.of("world");
+const s1$ = rxjs.interval(1000).pipe(ops.map((x) => "Stream 1: " + x));
+const s2$ = rxjs.interval(500).pipe(ops.map((x) => "Stream 2: " + x));
 
-// rxjs.merge(s1$, s2$).subscribe(createSubscribe("merge"));
-s1$.pipe(ops.merge(s2$)).subscribe(createSubscribe("merge"));
+rxjs.merge(s1$, s2$).pipe(ops.take(12)).subscribe(createSubscribe("merge"));
