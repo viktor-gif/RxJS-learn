dfc1e9e4e0da2ff444e1d2c0eed4ec7e756a0b1c
viktor-gif <klim.victor.ua@gmail.com>
2021-11-13 23:44:47 +0200

    RxJS: lessons on udemi to 31 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Observable} from 'rxjs';
 import { Header } from './componets/Header/Header';
 import { Main } from './componets/Main/Main';
 import { Nav } from './componets/Nav/Nav';
 import { Footer } from './componets/Footer/Footer';
+import { RxjsLearn } from './componets/rxjs-learn/RxjsLearn';
 
 const App = () => {
-
-  // const observable$ = new Observable<string>(subscriber => {
-  //   console.log('Observable executed');
-  //   subscriber.next('Alice');
-  //   setTimeout(() => subscriber.next('Ben'), 2000);
-  //   setTimeout(() => subscriber.next('Charlie'), 4000);
-  // });
-
-  // const subscription = observable$.subscribe(value => console.log(value));
-
-  // setTimeout(() => {
-  //   console.log('unsubscribe');
-  //   subscription.unsubscribe();
-  // }, 3000)
-
-  // const observable$ = new Observable<string>(subscriber => {
-  //   console.log('Observable executed');
-  //   subscriber.next('Alice');
-  //   setTimeout(() => subscriber.next('Ben'), 2000);
-  //   setTimeout(() => subscriber.next('Charlie'), 4000);
-  // });
-
-  // console.log('subscription 1 starts');
-  // observable$.subscribe(value => console.log('subscription 1: ', value));
-
-  // setTimeout(() => {
-  //   console.log('subscription 2 starts');
-  //   observable$.subscribe(value => console.log('subscription 2: ', value));
-  // }, 1000)
 
   return (
     <div className="App">
@@ -53,6 +24,7 @@ const App = () => {
         <footer className="footer">
           <Footer />
         </footer>
+        <RxjsLearn />
       </div>
     </div>
   );
---
+++ src/componets/rxjs-learn/RxjsLearn.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {Observable} from 'rxjs';
+
+export const RxjsLearn = () => {
+
+    const observable$ = new Observable<number>(subscriber => {
+        let counter = 0;
+        const intervalId = setInterval(() => {
+            console.log('Emitted ', counter);
+            subscriber.next(counter += 1);
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    });
+
+    const subscription = observable$.subscribe(val => console.log(val));
+
+    setTimeout(() => subscription.unsubscribe(), 7000);
+
+
+
+
+//   const observable$ = new Observable<string>(subscriber => {
+//     console.log('Observable executed');
+//     subscriber.next('Ivan');
+
+//     setTimeout(() => subscriber.error(new Error('Failure')), 1000);
+
+//     setTimeout(() => {
+//         subscriber.next('Alice');
+//         subscriber.complete();
+//     }, 2000)
+    
+//     setTimeout(() => subscriber.error(new Error('Failure')), 4000);
+
+//     subscriber.next('Vasya');
+
+//     return () => {
+//         console.log('Teardown');
+//     }
+    
+//   });
+
+//   console.log('Before');
+//   observable$.subscribe({
+//       next: val => console.log(val),
+//       error: err => console.log(err.message),
+//       complete: () => console.log('Complete')
+//   });
+//   console.log('after');
+
+
+
+
+
+
+
+
+    return <div></div>
+}
