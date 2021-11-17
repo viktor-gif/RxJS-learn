e3b1598259a81f182a784e18217dbf217dba15d4
viktor-gif <klim.victor.ua@gmail.com>
2021-11-17 07:51:26 +0200

    Observable of

--- src/componets/rxjs-learn/RxjsLearn.tsx
+++ src/componets/rxjs-learn/RxjsLearn.tsx
@@ -1,58 +1,35 @@
 import React from "react";
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 
 export const RxjsLearn = () => {
 
-    // const observable$ = new Observable<number>(subscriber => {
-    //     let counter = 0;
-    //     const intervalId = setInterval(() => {
-    //         console.log('Emitted ', counter);
-    //         subscriber.next(counter += 1);
-    //     }, 1000)
+    of('Alice', 'Ben', 'John').subscribe({
+        next: value => console.log(value),
+        complete: () => console.log('Completed')
+    })
 
-    //     return () => {
-    //         clearInterval(intervalId);
-    //     }
-    // });
+    //of realisation
+    const names$ = new Observable(subscriber => {
+        subscriber.next('Alice2');
+        subscriber.next('Ben2');
+        subscriber.next('John2');
+        subscriber.complete();
+    })
+    names$.subscribe({
+        next: value => console.log(value),
+        complete: () => console.log('Completed2')
+    })
 
-    // const subscription = observable$.subscribe(val => console.log(val));
-
-    // setTimeout(() => subscription.unsubscribe(), 7000);
-   
-        console.log('Emitted ');
-
-
-//   const observable$ = new Observable<string>(subscriber => {
-//     console.log('Observable executed');
-//     subscriber.next('Ivan');
-
-//     setTimeout(() => subscriber.error(new Error('Failure')), 1000);
-
-//     setTimeout(() => {
-//         subscriber.next('Alice');
-//         subscriber.complete();
-//     }, 2000)
-    
-//     setTimeout(() => subscriber.error(new Error('Failure')), 4000);
-
-//     subscriber.next('Vasya');
-
-//     return () => {
-//         console.log('Teardown');
-//     }
-    
-//   });
-
-//   console.log('Before');
-//   observable$.subscribe({
-//       next: val => console.log(val),
-//       error: err => console.log(err.message),
-//       complete: () => console.log('Complete')
-//   });
-//   console.log('after');
-
-
-
+    //creating my 'of'
+    function myOf(...args: any[]): Observable<any> {
+        return new Observable(subscriber => {
+            for (let i = 0; i < args.length; i++) {
+                subscriber.next(args[i]);
+            }
+            subscriber.complete();
+        })
+        
+    }
 
 
 
