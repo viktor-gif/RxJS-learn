b80d8fd09b2f2a39aa46397558283a339b48c182
viktor-gif <klim.victor.ua@gmail.com>
2021-11-18 22:34:23 +0200

    Observable combineLatest

--- src/componets/rxjs-learn/RxjsLearn.tsx
+++ src/componets/rxjs-learn/RxjsLearn.tsx
@@ -1,67 +1,54 @@
 import { rejects } from "assert";
-import React, { useEffect } from "react";
-import {forkJoin, from, fromEvent, interval, Observable, of, timer} from 'rxjs';
+import React, { useEffect, useState } from "react";
+import {combineLatest, forkJoin, from, fromEvent, interval, Observable, of, timer} from 'rxjs';
 import s from "./RxjsLearn.module.css";
 import {ajax} from "rxjs/ajax";
 
 export const RxjsLearn = () => {
+    const [help, setHelp] = useState(0);
    
     // forkJoin-Observable that emittes successfully
     useEffect(() => {
-        const randomName$ = ajax(
-            'https://random-data-api.com/api/name/random_name');
-    
-        const randomNation$ = ajax(
-            'https://random-data-api.com/api/nation/random_nation');
-    
-        const randomFood$ = ajax(
-            'https://random-data-api.com/api/food/random_food');
-    
-        //     //@ts-ignore
-        // randomName$.subscribe(sub => console.log(sub.response.first_name));
-        // //@ts-ignore
-        // randomNation$.subscribe(sub => console.log(sub.response.capital));
-        // //@ts-ignore
-        // randomFood$.subscribe(sub => console.log(sub.response.dish));
+        const input: any = document.getElementById('t-input');
+        const select: any = document.getElementById('conv-select');
+        const result = document.getElementById('result-text');
+        console.log(result?.innerText);
 
-        forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
-            ([nameAjax, nationAjax, foodAjax]) => console.log(
-                //@ts-ignore
-                `${nameAjax.response.first_name} is from ${nationAjax.response.capital} and likes to eat ${foodAjax.response.dish}.`
+        if (input && select && result) {
+            const inputEv = fromEvent(input, 'input');
+            const selectEv = fromEvent(select, 'input');
+
+            combineLatest([inputEv, selectEv]).subscribe(
+                ([temperature, conversion]: any[]) => {
+                    const temp = temperature.target.value;
+                    if (conversion.target.value === "f-to-c") {
+                            result.innerText = String(Number(temp - 32) * 5/9);
+                    } else if (conversion.target.value === "c-to-f") {
+                        result.innerText = String(Number(temp * 9/5 + 32));
+                    } else {
+                        result.innerText = "Please choose conversion";
+                    }
+                    console.log(temp, conversion.target.value)
+                }
             )
-        )
+        }
+        
         
     }, [])
 
-    // forkJoin-Observable with error
-    useEffect(() => {
-        // const a$ = new Observable(subscriber => {
-        //     setTimeout(() => {
-        //         subscriber.next('A');
-        //         subscriber.complete();
-        //     }, 3000)
-
-        //     return () => console.log('A teardown')
-        // })
-        // const b$ = new Observable(subscriber => {
-        //     setTimeout(() => {
-        //         subscriber.error('Failure!');
-        //     }, 5000)
-
-        //     return () => console.log('B teardown')
-        // })
-
-        // forkJoin([a$, b$]).subscribe({
-        //     next: val => console.log(val),
-        //     error: err => console.log('Error: ', err)
-        // })
-    }, [])
-
     
     
-
-
-
-
-    return <div><button className={s.btnClick} id="btnClick">Click</button></div>
+    return <div>
+        <div className={s.temperature}>
+            <input type="text" id="t-input" placeholder="Temperature" />
+            <br />
+            <select id="conv-select">
+                <option value="">Choose version</option>
+                <option value="f-to-c">F -&gt; C</option>
+                <option value="c-to-f">C -&gt; F</option>
+            </select>
+            <br />
+            <p id="result-text">Please fill the above form</p>
+        </div>
+    </div>
 }
