74eb38e73350d842c5323e2c22105df62de3516e
viktor-gif <klim.victor.ua@gmail.com>
2021-11-22 19:15:36 +0200

    operator catchError

--- src/componets/rxjs-learn/RxjsLearn.tsx
+++ src/componets/rxjs-learn/RxjsLearn.tsx
@@ -1,50 +1,30 @@
 import React, { useEffect } from "react";
-import { fromEvent, of} from 'rxjs';
-import { filter, map, tap, debounceTime } from 'rxjs/operators';
+import { EMPTY, fromEvent, Observable, of} from 'rxjs';
+import { filter, map, tap, debounceTime, catchError } from 'rxjs/operators';
 import s from "./RxjsLearn.module.css";
 
 export const RxjsLearn = () => {
-
-    interface NewsItem {
-        category: 'Business' | 'Sports';
-        content: string;
-    }
    
-    // tap
-    // useEffect(() => {
-    //     of(1, 7, 3, 6, 2)
-    //     .pipe(
-    //         filter(val => val > 5),
-    //         tap({
-    //             next: value => console.log('Spy: ', value)
-    //         }),
-    //         map(val => val * 2),
-    //     )
-    //     .subscribe(value => console.log('Output: ', value));
-
-        // Starting from RxJS 7.3.0, the tap() operator can do even more. You can see when the 
-        // Subscription starts and ends at the level of the tap() operator.
-        // of(1, 7, 3, 6, 2).pipe(
-        //       tap({
-        //         subscribe: () => console.log('New inner Subscription'),
-        //         unsubscribe: () => console.log('Unsubscribed'),
-        //         finalize: () => console.log('Inner Subscription ended')
-        //       })
-        //   ).subscribe(val => console.log(val)).unsubscribe();
-    // }, [])
-
-    //debounceTime
     useEffect(() => {
-        const slider: any = document.getElementById('slider');
-        console.log(slider)
+        const failingHttpRepuest$ = new Observable(subscriber => {
+            setTimeout(() => {
+                subscriber.error(new Error('Timeout'));
+            }, 3000)
+        })
 
-        fromEvent(slider, 'input')
-        .pipe(
-            debounceTime(1000),
-            // @ts-ignore
-            map((event) => event.target.value)
-        )
-        .subscribe(value => console.log(value))
+        console.log('App started');
+
+        failingHttpRepuest$.pipe(
+            catchError(err => of('Fallback value')),
+            
+            //empty observable
+            catchError(err => EMPTY)
+
+        ).subscribe({
+            next: val => console.log(val),
+            // error: err => console.log(err),
+            complete: () => console.log('complete')
+        })
     }, [])
     
     
